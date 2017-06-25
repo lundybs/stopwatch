0884143f66d8eb0bb85e75368e6a897303f65456
Brad Lundy <crusin67@hotmail.com>
2017-06-24 20:53:53 -0700

    fixed lap button to print lap times

--- stopwatch.js
+++ stopwatch.js
@@ -11,19 +11,20 @@ const stopwatchTime = document.querySelector('#stopwatchTime');
 //constant that should never change
 const laps = [];
 const intervalRate = 10;
+const liLap = document.createElement('li');
 
 //values that will change often
 let intervalId = null;
 let rawTime = 0;
 
 // turns the time into a human readable format
-function formatTime (raw) {
-  let seconds = Math.floor(raw / 1000)
-  let fractionalSeconds = (raw % 1000) / 1000
-  let minutes = Math.floor(seconds / 60)
-  seconds = seconds - (60 * minutes) + fractionalSeconds
+function formatTime(raw) {
+    let seconds = Math.floor(raw / 1000)
+    let fractionalSeconds = (raw % 1000) / 1000
+    let minutes = Math.floor(seconds / 60)
+    seconds = seconds - (60 * minutes) + fractionalSeconds
 
-  return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
+    return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
 function stopwatchStart(event) {
@@ -47,24 +48,32 @@ function stopwatchStop(event) {
 
 function stopwatchLap(event) {
     event.preventDefault();
-    console.log("lap");
-
-    lapList.innerHTML = formatTime(rawTime)
+    let printLapList = document.getElementById("lapList");
+    laps.push(formatTime(rawTime));
+    lapList.innerHTML = "";
+    for (var i = 0; i < laps.length; i++) {
+        lapNumber = i + 1;
+        nameList = "<li>" + "Lap "+ lapNumber + " time is " + laps[i] + "</li>";
+        document.getElementById("lapList").innerHTML += nameList;
+    }
 }
 
 function stopwatchReset(event) {
+    clearInterval(intervalId);
     stopwatchTime.innerHTML = formatTime("0");
     lapList.innerHTML = "";
 }
 
 // adds a leading zero because humans like them
-function zeroPad (value) {
-  let pad = value < 10 ? '0' : ''
-  return `${pad}${value}`
+function zeroPad(value) {
+    let pad = value < 10 ? '0' : ''
+    return `${pad}${value}`
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log('ready!')
+    stopwatchTime.innerHTML = formatTime(rawTime);
+
 
     start.addEventListener("click", stopwatchStart);
     stop.addEventListener("click", stopwatchStop);
